564fcc0e741d3b5f9d7b4152496e1714e2c014cd
petru <smart__petea@mail.ru>
2016-08-22 09:14:44 +0300

    two_pluses.

--- Algorithms/Implementation/two_pluses.java
+++ Algorithms/Implementation/two_pluses.java
@@ -1,11 +1,40 @@
 //https://www.hackerrank.com/challenges/two-pluses
 //
 import java.util.Scanner;
+import java.util.List;
+import java.util.LinkedList;
 
 class Cross {
+    private static List<Cross> crosses = new LinkedList<>();
+    private static char[][] table;
+    private static int N;
+    private static int M;
+
     int i;
     int j;
     int size;
+
+    private static void fillTable() {
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+
+        for(int i = 0; i < N; i++) table[i] = scanner.next().toCharArray();
+    }
+
+    public static int process() {
+        fillTable();
+
+        for(int i = 0; i < N; i++) 
+            for(int j = 0; j < M; j++)
+                if(table[i][j] == 'G') generateCrosses(i, j);
+
+        return getMaximum();
+    }
+
+    public static int getMaximum() {
+        return -1;
+    }
 
     Cross(int x, int y, int sz) {
         i = x;
@@ -13,65 +42,18 @@ class Cross {
         size = sz;
     }
 
-    public boolean overlap(Cross crs) {
-        return false;
-    }
-
-    static public Cross getCross(int x, int y, char[][] table) {
-        int i = 1;
+    private static void generateCrosses(int x, int y) {
+        int i = 0;
 
         int height = table.length;
         int width = table[0].length;
 
-        while(x - i > 0 && x + i < height && y - i > 0 && y + i < width && table[x - i][y] == 'G' && table[x + i][y] == 'G' && table[x][y - i] == 'G' && table[x][y + i] == 'G') ++i;
-
-        return new Cross(x, y, i);
+        while(x - i > 0 && x + i < height && y - i > 0 && y + i < width && table[x - i][y] == 'G' && table[x + i][y] == 'G' && table[x][y - i] == 'G' && table[x][y + i] == 'G') crosses.add(new Cross(x, y, ++i));
     }
 }
 
 public class two_pluses {
     static public void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
-
-        char[][] table = new char[N][M];
-        int i, j;
-
-        for(i = 0; i < N; i++) table[i] = scanner.next().toCharArray();
-
-        Cross frstCross = null;
-        Cross scndCross = null;
-        Cross tempCross;
-
-        i = 0;
-        j = 0;
-        int size = 0;
-        while(i < N - size) {
-            j = size;
-            while(j < M - size) {
-                if(table[i][j] == 'G') {
-                    tempCross = Cross.getCross(i, j, table);
-
-                    if(frstCross == null) {
-                        frstCross = tempCross;
-                    } else if(scndCross == null) {
-                        if(!frstCross.overlap(tempCross)) {
-                            if(frstCross.size > tempCross.size) {
-                                scndCross = frstCross;
-                                frstCross = tempCross;
-                            } else {
-                                scndCross = tempCross;
-                            }
-                        }
-                    } else {
-
-                    }
-                }
-                ++j;
-            }
-
-            i = Math.max(++i, size);
-        }
+        System.out.println(Cross.process());
     }
 }
